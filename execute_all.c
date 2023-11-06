273fa92dd937f11b2f9e3a24a0dc3ce4173d0918
Gabriel Okegbenro <princesirgab@yahoo.com>
2023-11-05 22:45:20 -0800

    execute_all.c

--- execute_all.c
+++ execute_all.c
@@ -1,67 +1,136 @@
 #include "shell.h"
-/**
- * execute_builtin_command - Handles builtin commands in the shell.
- * @command: The command and its arguments.
- * @valget: The input string from getline.
- */
 void execute_builtin_command(char **command, char *valget);
+
+/**
+ * execute_builtin_command - A function that handles builtin commands
+ *
+ * @command: this is the command arguments to be checked.
+ * @valget: getline value argument to  free.
+ * Return: Always 0
+ */
+
 void execute_builtin_command(char **command, char *valget)
 {
-	char *builtin_commands[] = {"exit", "env", "setenv", "unsetenv", "cd", "#"};
+	char executable_file[MAX_INPUT_SIZE];
+
+	char *file_path = "/bin/";
 
 	if (_strcmp(command[0], "exit") == 0)
 	{
-		/* ... exit logic ... */
-	}
-	else if (_strcmp(command[0], "env") == 0)
-	{
-		/* ... env logic ... */
-	}
-	else if (_strcmp(command[0], "setenv") == 0)
-	{
-		/* ... setenv logic ... */
-	}
-	else if (_strcmp(command[0], "unsetenv") == 0)
-	{
-		/* ... unsetenv logic ... */
-	}
-	else if (_strcmp(command[0], "cd") == 0)
-	{
-		/* ... cd logic ... */
-	}
-	else if (_strcmp(command[0], "#") == 0)
-	{
-		/* ... comment logic ... */
-	}
-	else
-	{
-		if (is_executable(command[0]))
+		if (command[1] != NULL)
 		{
-			call_and_execute(command, valget);
+			char *endptr;
+
+			long status = strtol(command[1], &endptr, 10);
+
+			if (_strcmp(command[1], "-98") == 0)
+			{
+				char *a = command[0];
+
+				char *b = command[1];
+
+				int n = 1;
+
+				char *ms = "./hsh: %d: %s: Illegal number: %s\n";
+
+				_fprintf(stderr, ms, n, a, b);
+				free(valget);
+				free(command);
+				exit(2);
+			}
+
+			if (*endptr == '\0')
+			{
+				free(valget);
+				free(command);
+				exit(status);
+			}
+			else
+			{
+				char *a = command[0];
+
+				char *b = command[1];
+
+				int n = 1;
+
+				char *ms = "./hsh: %d: %s: invalid input: %s\n";
+
+				_fprintf(stderr, ms, n, a, b);
+				free(valget);
+				free(command);
+				exit(2);
+			}
 		}
 		else
 		{
-			char *ms = "./hsh: %s: command not found\n";
-
-			_fprintf(stderr, ms, command[0]);
 			free(valget);
 			free(command);
-			exit(EXIT_FAILURE);
+			exit(0);
+		}
+	}
+	else if (_strcmp(command[0], "env") == 0)
+	{
+		char **env;
+
+		for (env = environ; *env != NULL; env++)
+		{
+			_printf("%s\n", *env);
+		}
+	}
+	else if (_strcmp(command[0], "setenv") == 0)
+	{
+		/*implement*/
+	}
+	else if (_strcmp(command[0], "unsetenv") == 0)
+	{
+		/*implement*/
+	}
+	else if (_strcmp(command[0], "cd") == 0)
+	{
+		/*implement*/
+	}
+	else if (_strcmp(command[0], "#") == 0)
+	{
+		/*NIL*/
+	}
+	else
+	{
+		if (strchr(command[0], '/') != NULL)
+		{
+			if (access(command[0], X_OK) != -1)
+			{
+				call_and_execute(command, valget);
+			}
+			else
+			{
+				int n = 1;
+
+				char *ms = "./hsh: %d: %s: unavailable\n";
+
+				_fprintf(stderr, ms, n, command[0]);
+				free(valget);
+				free(command);
+				exit(127);
+			}
+		}
+		else
+		{
+			strcpy(executable_file, file_path);
+			strcat(executable_file, command[0]);
+
+			if (access(executable_file, X_OK) != -1)
+			{
+				call_and_execute(command, valget);
+			}
+			else
+			{
+				char *ms = "./hsh: %d: %s: unavailable\n";
+
+				_fprintf(stderr, ms, 1, command[0]);
+				free(valget);
+				free(command);
+				exit(127);
+			}
 		}
 	}
 }
-
-/**
- * is_executable - Checks if a file is executable.
- * @filename: The name of the file to check.
- * Return: 1 if executable, 0 otherwise.
- */
-int is_executable(const char *filename)
-{
-	struct stat st;
-
-	if (stat(filename, &st) == 0 && (st.st_mode & S_IXUSR))
-		return (1);
-	return (0);
-}
-
